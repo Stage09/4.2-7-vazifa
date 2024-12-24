a11672a66ea323928b74798c85fa1ba46811a222
Stage777999 <ikingib1979@gmail.com>
2024-12-24 20:58:20 +0500

    finished

---
+++ main.js
@@ -0,0 +1,44 @@
+https://the-winter.github.io/codingjs/exercise.html?name=sumDouble&title=Warmup-1
+
+// function sumOrDouble(a, b) {
+//     if (a === b) {
+//       return 2 * (a + b); 
+//     } else {
+//       return a + b;  
+//     }
+//   }
+
+//   console.log(sumOrDouble(3, 4)); 
+//   console.log(sumOrDouble(5, 5)); 
+  
+
+
+https://the-winter.github.io/codingjs/exercise.html?name=nearHundred&title=Warmup-1
+
+// function nearHundred(n) {
+//     return (Math.abs(n - 100) <= 10) || (Math.abs(n - 200) <= 10);
+// }
+
+// console.log(nearHundred(93)); 
+// console.log(nearHundred(90)); 
+// console.log(nearHundred(89));  
+
+https://the-winter.github.io/codingjs/exercise.html?name=front22&title=Warmup-1
+
+// function front22(str) {
+//     let front = str.slice(0, 2);
+//     return front + str + front;   
+//   }
+//   console.log(front22('kitten'));  
+//   console.log(front22('Ha'));      
+//   console.log(front22('abc'));     
+    
+https://the-winter.github.io/codingjs/exercise.html?name=hasTeen&title=Warmup-1
+
+// function hasTeen(a, b, c) {
+//     return (a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19);
+//   }
+//   console.log(hasTeen(13, 20, 10));  
+//   console.log(hasTeen(20, 19, 10));  
+//   console.log(hasTeen(20, 10, 13));  
+    
